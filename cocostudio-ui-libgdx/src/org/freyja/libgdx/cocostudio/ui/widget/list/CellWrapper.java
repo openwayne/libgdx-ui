56121d615d016e7a5cfab89bfc3e1b2159d4aea7
openwayne <openwayne@gmail.com>
2014-05-06 21:18:54 +0800

    修正cell相关问题

---
+++ cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/list/CellWrapper.java
@@ -0,0 +1,57 @@
+package org.freyja.libgdx.cocostudio.ui.widget.list;
+
+import org.freyja.libgdx.cocostudio.ui.CocoStudioUIEditor;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+public abstract class CellWrapper {
+	protected CocoStudioUIEditor _editor;
+	protected Group _group;
+	protected int _idx;
+
+	public void init(FileHandle file) {
+		_editor = new CocoStudioUIEditor(file, null, null, null, null);
+		_group = _editor.createGroup();
+	}
+
+	public CocoStudioUIEditor getEditor() {
+		return _editor;
+	}
+
+	public void setPosition(float x, float y) {
+		_group.setPosition(x, y);
+	}
+
+	public void setIndex(int idx) {
+		_idx = idx;
+	}
+	
+	public int getIndex() {
+		return _idx;
+	}
+
+	public Group getGroup() {
+		return _group;
+	}
+
+	/**
+	 * 设置数据 记得调用init方法
+	 * 
+	 * @param data
+	 */
+	public abstract void setData(Object data);
+
+	/**
+	 * 复制自身
+	 */
+	public abstract CellWrapper cloneCell();
+
+	public void selectCell() {
+		System.out.println("点中" + _idx + "sell了");
+	}
+	
+	public void cancleCell() {
+		System.out.println("取消" + _idx + "选中");
+	}
+}
--- cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/list/ICellModel.java
+++
@@ -1,8 +0,0 @@
-package org.freyja.libgdx.cocostudio.ui.widget.list;
-
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
-public interface ICellModel {
-	public void setData();
-	public Table createCell();
-}
