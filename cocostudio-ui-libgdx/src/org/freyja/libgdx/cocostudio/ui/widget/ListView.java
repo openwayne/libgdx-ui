1b1460635c64e8d6261d759e62ba4d62af6660a5
openwayne <openwayne@gmail.com>
2014-05-06 11:59:13 +0800

    it is only for backup.i will rework it

--- cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/ListView.java
+++ cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/ListView.java
@@ -1,23 +1,18 @@
 package org.freyja.libgdx.cocostudio.ui.widget;
 
-import org.freyja.libgdx.cocostudio.ui.widget.list.BaseListCell;
-import org.freyja.libgdx.cocostudio.ui.widget.list.CellClickedListener.CellClickedEvent;
-import org.freyja.libgdx.cocostudio.ui.widget.list.CellHelper.CellWrapper;
+import org.freyja.libgdx.cocostudio.ui.widget.list.CellWrapper;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
-import com.badlogic.gdx.scenes.scene2d.utils.Cullable;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Pools;
-import com.badlogic.gdx.utils.SnapshotArray;
 
 /**
  * 一个支持多列的列表 现在的优化不成功，消耗会大 默认每个格子的space是5
@@ -25,11 +20,11 @@ import com.badlogic.gdx.utils.SnapshotArray;
  * @author jianxiang.zi
  * 
  */
-public class ListView extends Table implements Cullable {
-	private ListStyle style;
-	private Object[] items;
-	private int selectedIndex;
-	private Rectangle cullingArea;
+public class ListView extends ScrollPane {
+	private ScrollPaneStyle style;
+	private Object[] items = new Object[1];
+	private Array<CellWrapper> cells = new Array<CellWrapper>();
+	private int selectedIndex = 0;
 	private float prefWidth, prefHeight;
 	// row行数，希望一次看见的行数，实际上现在是通过遮挡来实现的,lines是根据数据算出来的行数
 	private int col = 1;
@@ -39,128 +34,15 @@ public class ListView extends Table implements Cullable {
 	// 两个元素之间的垂直间距
 	private float vSpace = 5;
 	private float hSpace = 5;
-	private Array<Integer> selectedIndecies;
-	private boolean multiSelectable;
 	private boolean selectable = true;
 	private boolean isDirty = false;
 	private boolean isSelectedDirty = false;
-	/**
-	 * 那几个cell的容器，这样用来保证cell是放在下面的
-	 */
-	private Table cellTable;
 	private int hAlign = Align.center;
+	private Table cellTable;
 
-	public ListView(ListStyle style) {
-		setStyle(style);
-		selectedIndecies = new Array<Integer>();
-		cellTable = new Table();
-		cullingArea = new Rectangle();
-		addActor(cellTable);
-
-		addCaptureListener(new InputListener() {
-
-			@Override
-			public void touchUp(InputEvent event, float x, float y,
-					int pointer, int button) {
-			}
-
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				if (pointer == 0 && button != 0)
-					return false;
-				return true;
-			}
-		});
-	}
-
-	int oldIndex;
-
-	void touchUp(float x, float y) {
-		oldIndex = selectedIndex;
-		Array<Integer> oldIndecies = new Array<Integer>();
-		System.arraycopy(selectedIndecies.items, 0, oldIndecies.items, 0,
-				selectedIndecies.size);
-		// 点中的行
-		int r = (int) ((getHeight() - y) / (itemHeight + vSpace));
-		// 点中的列
-		int c = (int) (x / (itemWidth + hSpace));
-		selectedIndex = r * col + c;
-		if (selectedIndex < 0 || selectedIndex > items.length - 1) {
-			selectedIndex = oldIndex;
-			return;
-		}
-		// selectedIndex = Math.max(0, selectedIndex);
-		// selectedIndex = Math.min(items.length - 1, selectedIndex);
-		System.out.println("选中index:" + selectedIndex);
-		if (this.multiSelectable) {
-			// 多选情况下的处理
-			if (selectedIndecies.contains(selectedIndex, true)) {
-				selectedIndecies.removeValue(selectedIndex, true);
-			} else {
-				selectedIndecies.add(selectedIndex);
-			}
-		}
-		ChangeEvent changeEvent = Pools.obtain(ChangeEvent.class);
-		if (fire(changeEvent)) {
-			// 事件被取消，单选的时候回复原来的选择
-			selectedIndex = oldIndex;
-			if (multiSelectable) {
-				selectedIndecies = oldIndecies;
-			}
-		}
-		if (selectable) {
-			// 更新那个选中状态
-			isDirty = true;
-		}
-		Pools.free(changeEvent);
-	}
-
-	public void cellClicked(BaseListCell cell) {
-		int oldIndex = selectedIndex;
-		Array<Integer> oldIndecies = new Array<Integer>();
-		oldIndecies.addAll(selectedIndecies);
-
-		selectedIndex = cell.getIndex();
-
-		if (this.multiSelectable) {
-			// 多选情况下的处理
-			if (selectedIndecies.contains(selectedIndex, true)) {
-				selectedIndecies.removeValue(selectedIndex, true);
-			} else {
-				selectedIndecies.add(selectedIndex);
-			}
-		}
-		ChangeEvent changeEvent = Pools.obtain(ChangeEvent.class);
-		if (fire(changeEvent)) {
-			// 事件被取消，单选的时候回复原来的选择
-			selectedIndex = oldIndex;
-			if (multiSelectable) {
-				System.arraycopy(oldIndecies.items, 0, selectedIndecies.items,
-						0, oldIndecies.size);
-			}
-		}
-		if (selectable) {
-			// 更新那个选中状态
-			isSelectedDirty = true;
-		}
-		Pools.free(changeEvent);
-
-		CellClickedEvent clickEvent = Pools.obtain(CellClickedEvent.class);
-		clickEvent.cell = cell;
-		fire(clickEvent);
-		Pools.free(clickEvent);
-	}
-
-	public void setMultiSelectable(boolean value) {
-		this.multiSelectable = value;
-		// 刚转换成多选的时候要清掉
-		if (multiSelectable) {
-			this.selectedIndecies.clear();
-		} else if (items.length > 0) {
-			// 由多选转换成单选的时候给个默认选中
-			// this.setSelectedIndex(0);
-		}
-		isSelectedDirty = true;
+	public ListView(ScrollPaneStyle style) {
+		super(new Table(), style);
+		cellTable = (Table) getWidget();
 	}
 
 	public void setSelectable(boolean value) {
@@ -168,11 +50,10 @@ public class ListView extends Table implements Cullable {
 		isSelectedDirty = true;
 	}
 
-	public void setStyle(ListStyle style) {
+	public void setStyle(ScrollPaneStyle style) {
 		if (style == null)
 			throw new IllegalArgumentException("style cannot be null.");
 		this.style = style;
-		this.pad(0);
 		invalidateHierarchy();
 	}
 
@@ -180,78 +61,63 @@ public class ListView extends Table implements Cullable {
 	 * Returns the list's style. Modifying the returned style may not have an
 	 * effect until {@link #setStyle(ListStyle)} is called.
 	 */
-	public ListStyle getStyle() {
+	public ScrollPaneStyle getStyle() {
 		return style;
 	}
 
-	/**
-	 * 用了裁剪的方法
-	 */
-	private void updateItem2() {
-		SnapshotArray<Actor> cellList = cellTable.getChildren();
-		for (int i = 0; i < cellList.size; i++) {
-			Actor tmp = cellList.get(i);
-
-			float tmpY = getItemY(i);
-			// 看不见的部分的优化处理
-			if (cullingArea == null
-					|| (tmpY < cullingArea.y + cullingArea.height && tmpY
-							+ itemHeight > cullingArea.y)) {
-				tmp.setVisible(true);
-			} else if (tmpY + itemHeight < cullingArea.y) {
-				tmp.setVisible(false);
-				;
-			}
-		}
-	}
-
-	private void updateSelected() {
-		for (Actor actor : cellTable.getChildren()) {
-			if (!(actor instanceof BaseListCell)) {
+	private Vector2 hitPot = new Vector2();
+	private Rectangle hitRect = new Rectangle();
+	private int oldIndex;
+	@Override
+	public Actor hit(float x, float y, boolean touchable) {
+		hitRect.set(0, 0, getWidth(), getHeight());
+		if (touchable && getTouchable() == Touchable.disabled)
+			return null;
+		for (int i = 0; i < cells.size; i++) {
+			CellWrapper child = cells.get(i);
+			Actor dos = child.getGroup();
+			if (!dos.isVisible() || !hitRect.contains(hitPot))
 				continue;
-			}
-			BaseListCell tmp = (BaseListCell) actor;
-			if (selectable) {
-				if (multiSelectable) {
-					if (selectedIndecies.contains(tmp.getIndex(), true)) {
-						tmp.setSelected(true);
-					} else {
-						tmp.setSelected(false);
-					}
-				} else if (tmp.getIndex() == selectedIndex) {
-					tmp.setSelected(true);
-				} else {
-					tmp.setSelected(false);
-				}
+			dos.parentToLocalCoordinates(hitPot.set(x, y));
+			
+			Actor hit = dos.hit(hitPot.x, hitPot.y, touchable);
+			if (hit != null) {
+				CellWrapper oldCell = cells.get(selectedIndex);
+				oldCell.cancleCell();
+				child.selectCell();
+				selectedIndex = child.getIndex();
+				return hit;
 			}
 		}
+
+		return null;
 	}
 
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
 		if (isDirty) {
-			updateItem2();
+			updateItems();
 			isDirty = false;
-			isSelectedDirty = true;
 		}
+
 		if (isSelectedDirty) {
-			updateSelected();
 			isSelectedDirty = false;
 		}
 		super.draw(batch, parentAlpha);
 	}
 
-	/**
-	 * 选中的数据
-	 * 
-	 * @return
-	 */
-	public Object[] getSelections() {
-		Object[] retItems = new Object[selectedIndecies.size];
-		for (int i = 0; i < retItems.length; i++) {
-			retItems[i] = items[selectedIndecies.get(i)];
+	private void updateItems() {
+		selectedIndex = 0;
+		for (int i = 0; i < cells.size; i++) {
+			try {
+				CellWrapper t = cells.get(i);
+				t.setIndex(i);
+				t.setPosition(getItemX(i), getItemY(i));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		return retItems;
+		computeSize();
 	}
 
 	/**
@@ -279,27 +145,6 @@ public class ListView extends Table implements Cullable {
 		return oldIndex;
 	}
 
-	public Integer[] getSelectedIndecies() {
-		return selectedIndecies.toArray(Integer.class);
-	}
-
-	public void setSelectedIndecies(Integer[] indecies) {
-		selectedIndecies.clear();
-		if (indecies.length > 0)
-			selectedIndecies.addAll(indecies);
-		isSelectedDirty = true;
-	}
-
-	public void setSelections(Object[] its) {
-		selectedIndecies.clear();
-		for (int i = 0, n = its.length; i < n; i++) {
-			if (indexOf(items, its[i], true) >= 0) {
-				selectedIndecies.add(i);
-			}
-		}
-		isSelectedDirty = true;
-	}
-
 	/**
 	 * @return 返回被选中的那个数据.
 	 */
@@ -322,23 +167,26 @@ public class ListView extends Table implements Cullable {
 		return selectedIndex;
 	}
 
-	public void setItems(Object[] objects, CellWrapper cell, int itemWidth, int itemHeight) {
+	public void setItems(Object[] objects, CellWrapper cell, int itemWidth,
+			int itemHeight) {
 		if (objects == null)
 			throw new IllegalArgumentException("items cannot be null.");
-		
+
 		this.itemWidth = itemWidth;
 		this.itemHeight = itemHeight;
 		items = objects.clone();
 		selectedIndex = 0;
 		cellTable.clear();
+		cells.clear();
 		for (int i = 0; i < items.length; i++) {
 			try {
 				CellWrapper t = cell.cloneCell();
 				t.setIndex(i);
-				t.setSize(itemWidth, itemHeight);
 				t.setData(items[i]);
+				t.getGroup().setSize(itemWidth, itemHeight);
 				t.setPosition(getItemX(i), getItemY(i));
-				cellTable.addActor(t);
+				cellTable.addActor(t.getGroup());
+				cells.add(t);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -346,6 +194,12 @@ public class ListView extends Table implements Cullable {
 
 		isDirty = true;
 		computeSize();
+	}
+
+	@Override
+	public void setSize(float width, float height) {
+		cellTable.setSize(width, height);
+		super.setSize(width, height);
 	}
 
 	private void computeSize() {
@@ -414,15 +268,6 @@ public class ListView extends Table implements Cullable {
 		return prefHeight;
 	}
 
-	public void setCullingArea(Rectangle culling) {
-		if (cullingArea.x != culling.x || cullingArea.y != culling.y
-				|| cullingArea.width != culling.width
-				|| cullingArea.height != culling.height) {
-			cullingArea.set(culling);
-			isDirty = true;
-		}
-	}
-
 	/**
 	 * 获取被选中的cell的在列表中的y值（从左下角开始算起）
 	 * 
@@ -489,7 +334,6 @@ public class ListView extends Table implements Cullable {
 	public void setHAlignment(int align) {
 		this.hAlign = align;
 	}
-
 
 	/**
 	 * The style for a list, see {@link List}.
