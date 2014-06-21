838abac45fc3ae7fd03f89127939478541cf0c37
xingxing.zhong <xingxing.zhong@helin-corp.com>
2014-06-21 11:18:57 +0800

    image设置文字的时候不居中的问题

--- cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/AdvancedImageButton.java
+++ cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/widget/AdvancedImageButton.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
@@ -60,6 +61,7 @@ public class AdvancedImageButton extends ImageButton {
 			return;
 		}
 		label.setText(txt);
+		label.setAlignment(Align.center);
 		label.setPosition((getWidth() - label.getWidth()) / 2,
 				(getHeight() - label.getHeight()) / 2);
 	}
